38621e3dc4809750aa8e5818fb279831c916582b
Valentin Ilas <valentin.ilas@gmail.com>
2020-08-30 21:01:22 +0200

    added bulma scss framework & first few components

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './components/App/App.vue'
+
+
+/**
+ * =============================================
+ * Import Boostrap vue
+ */
+require('@/assets/bulma.scss');
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
