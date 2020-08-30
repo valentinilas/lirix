6f0ebe8bedec4819d86f0b7fbe931dc46e0df853
Valentin Ilas <valentin.ilas@gmail.com>
2020-08-31 00:16:12 +0200

    added vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './components/App/App.vue'
-
+import { store } from './store/store';
 
 /**
  * Import Bulma scss
@@ -10,5 +10,6 @@ require('@/assets/bulma.scss');
 Vue.config.productionTip = false
 
 new Vue({
+    store: store,
     render: h => h(App),
 }).$mount('#app')
